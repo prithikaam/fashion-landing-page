19f1a6712f35f184cd124071a3184a33b922f6a8
prithikaam <prithikaam@gmail.com>
2026-04-19 15:13:38 +0530

    Serve frontend in production

--- server/index.js
+++ server/index.js
@@ -21,15 +21,15 @@ app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/products', require('./routes/productRoutes'));
 app.use('/api/orders', require('./routes/orderRoutes'));
 
-app.get('/', (req, res) => {
-  res.send('API is running...');
-});
-
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/dist")));
 
   app.get(/(.*)/, (req, res) => {
     res.sendFile(path.resolve(__dirname, "../client/dist/index.html"));
+  });
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running...');
   });
 }
 
